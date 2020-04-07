1d6010ee73d7bbf8b5b3ee7dcdd9ff46ca164259
Anastasiya Babiy <nastasia909@gmail.com>
2020-04-07 03:05:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int Balance = 2_000_000_000;
+        int Operation = 500_000_000;
+        int TotalBalance = Balance + Operation;
+        System.out.println(TotalBalance);
+    }
+}
